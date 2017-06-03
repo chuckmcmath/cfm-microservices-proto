4f2570d5bd5b859670c5ea4e7b449d24771576f2
Chuck McMath <chuck.mcmath@gmail.com>
2017-06-03 10:16:43 -0400

    Minor cleanup driven by looking in the problems tab.

--- service-one/src/main/java/com/mcmath/serviceone/StubInfoContributor.java
+++ service-one/src/main/java/com/mcmath/serviceone/StubInfoContributor.java
@@ -1,6 +1,5 @@
 package com.mcmath.serviceone;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
--- service-two/src/main/java/com/mcmath/servicetwo/S2InfoContributor.java
+++ service-two/src/main/java/com/mcmath/servicetwo/S2InfoContributor.java
@@ -1,6 +1,5 @@
 package com.mcmath.servicetwo;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
